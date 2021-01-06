a390b5869f4ed8051b0f5bec5347747ee2c7659a
etiwitty <atiyeh.keshavarz@gmail.com>
2021-01-06 10:50:35 -0800

    variables annotations

---
+++ Annotations/variables.ts
@@ -0,0 +1,8 @@
+const apples: number = 5;
+const speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+let now: Date = new Date();
