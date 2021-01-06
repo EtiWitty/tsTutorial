929d5b5339ffe83182c0bf8a7d192fac56101dcc
etiwitty <atiyeh.keshavarz@gmail.com>
2021-01-06 10:59:42 -0800

    type annotation for obj, arr, and obj literal

--- Annotations/variables.ts
+++ Annotations/variables.ts
@@ -5,4 +5,21 @@ let hasName: boolean = true;
 let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
+//build in objects
 let now: Date = new Date();
+
+//Array
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let truth: boolean[] = [true, true, false];
+
+//Class
+
+class Car {}
+let car: Car = new Car();
+
+//Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
