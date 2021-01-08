8dbec4f7bc9093e344b006b0801e075336be82be
etiwitty <atiyeh.keshavarz@gmail.com>
2021-01-07 17:19:44 -0800

    Void and Never

--- Annotations/functions.ts
+++ Annotations/functions.ts
@@ -9,3 +9,22 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number) {
   return a * b;
 };
+
+// when we dont want to use return statement, we use void
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+// when we dont expect function to return anything ever
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+// here, since we have return statement anyway, so annotation would be string
+const throwNewError = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  } else {
+    return message;
+  }
+};
