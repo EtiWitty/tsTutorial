716ffc7fdd9b9e833e2cc1887c59a646ac393994
etiwitty <atiyeh.keshavarz@gmail.com>
2021-01-09 22:58:44 -0800

    corner cases around arrays

--- Annotations/arrays.ts
+++ Annotations/arrays.ts
@@ -1,3 +1,20 @@
 const carMakers: string[] = ["ford", "toyota", "chevy"];
 const dates: Date[] = [new Date(), new Date()];
 const carsByMake: string[][] = [["f150"], ["corolla"], ["camaro"]];
+
+// Help with inference when excracting values
+// TS can do type inference when extracting values from an array
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+// TS can prevent us from adding incompatible values to the array
+carMakers.push(100);
+
+// Help with 'map'
+// We can help with 'map', 'forEach', 'reduce' functions
+carMakers.map(
+  (car: string): String => {
+    return car.toUpperCase();
+  }
+);
